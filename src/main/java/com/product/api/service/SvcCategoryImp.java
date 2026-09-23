b5c3dce0c8255dad52e985cd31a713e5a18b823d
Alexis Corona <alexiscorona@ciencias.unam.mx>
2026-09-23 11:26:10 -0600

    fix: corregir implementación de los métodos de SvcCategoryImp

--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -1,28 +1,40 @@
-import org.springframework.stereotype.Service;
-import com.product.exception.DBAccessException;
+package com.product.api.service;
+
+import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
 import com.product.exception.DBAccessException;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
-public class SvcCategory implements SvcCategory{
+public class SvcCategoryImp implements SvcCategory {
 
     final RepoCategory repo;
-    SvcCategoryImp(RepoCategory repo){
+
+    public SvcCategoryImp(RepoCategory repo) {
         this.repo = repo;
     }
 
-    @Override 
-    public ResponseEntity<List<Category>>getCategories(){
-        try{
-            return new ResponseEntity<>(repo.getCategories(),HttpStatus.OK);
-        }catch(DataAccessException e){
+    @Override
+    public ResponseEntity<List<Category>> getCategories() {
+        try {
+            return new ResponseEntity<List<Category>>(repo.getCategories(), HttpStatus.OK);
+        } catch(DataAccessException e) {
             throw new DBAccessException(e);
         }
-        
     }
 
-    @Override 
-    public ResponseEntity<List<Category>> getActiveCategories(){
-        return new ResponseEntity(repo.findByStatusOrderByCategory(1),HttpStatus.OK);
+    @Override
+    public ResponseEntity<List<Category>> getActiveCategories() {
+        try {
+            return new ResponseEntity<List<Category>>(repo.findByStatusOrderByCategory(1), HttpStatus.OK);
+        } catch(DataAccessException e) {
+            throw new DBAccessException(e);
+        }
     }
 }
